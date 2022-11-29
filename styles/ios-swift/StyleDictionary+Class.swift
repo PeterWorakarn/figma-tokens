9511a730874b14868d0e3cffbc17bc2493d33ff0
Worakarn Oun <skboardgames@gmail.com>
2022-11-29 18:32:28 +0700

    tailwind

---
+++ styles/ios-swift/StyleDictionary+Class.swift
@@ -0,0 +1,23 @@
+
+//
+// StyleDictionary+Class.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 29 Nov 2022 11:31:39 GMT
+
+
+import UIKit
+
+public class StyleDictionaryClass {
+    public static let globalNeutral00 = #2f2f2f
+    public static let globalNeutral01 = #464646
+    public static let globalNeutral02 = #d5d5d5
+    public static let globalNeutral03 = #f4f4f4
+    public static let globalNeutral04 = #ffffff
+    public static let globalPrimary00 = #323004
+    public static let globalPrimary01 = #756f0e
+    public static let globalPrimary02 = #fcf14d
+    public static let globalPrimary03 = #fdf47a
+    public static let globalPrimaryDefault = #fbed21
+}
---
+++ styles/ios-swift/StyleDictionary+Struct.swift
@@ -0,0 +1,23 @@
+
+//
+// StyleDictionary+Struct.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 29 Nov 2022 11:31:39 GMT
+
+
+import UIKit
+
+internal struct StyleDictionaryStruct {
+    internal static let globalNeutral00 = #2f2f2f
+    internal static let globalNeutral01 = #464646
+    internal static let globalNeutral02 = #d5d5d5
+    internal static let globalNeutral03 = #f4f4f4
+    internal static let globalNeutral04 = #ffffff
+    internal static let globalPrimary00 = #323004
+    internal static let globalPrimary01 = #756f0e
+    internal static let globalPrimary02 = #fcf14d
+    internal static let globalPrimary03 = #fdf47a
+    internal static let globalPrimaryDefault = #fbed21
+}
